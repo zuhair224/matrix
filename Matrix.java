74d42e2dd816c29203bf8fc99337925426f19029
Zuhair -Alzekri <75549570+zuhair224@users.noreply.github.com>
2021-05-01 18:07:08 +0200

    Create Matrix.java

---
+++ Matrix.java
@@ -0,0 +1,25 @@
+public class Matrix {
+    public static void main(String[] args){
+
+        //creating two matrices
+        int a[][]={{1,3,4 },{2,4,3},{3,4,5}};
+        int b[][]={{1,3,4 },{2,4,3},{1,2,4}};
+
+        //creating matrix to score the sum of the two matrices
+
+        int c[][]=new int [3][3];
+
+        //adding and printing addition of 2 matrices
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                c[i][j]=a[i][j]+b[i][j];
+
+                System.out.println(c[i][j]+" ");
+
+            }
+            System.out.println();
+        }
+
+    }
+
+}
